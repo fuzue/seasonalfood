2df827a5a04fc261fec94bcb10bf6a63d12c0038
Eduardo Liron <eduardoliron@gmail.com>
2023-07-14 10:31:52 -0300

    fix(FoodPage): fix transition on searchbar

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -76,6 +76,7 @@ export default function HeaderBar(props: Props) {
       padding: theme.spacing(1, 1, 1, 0),
       // vertical padding + font size from searchIcon
       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
       width: "100%",
       [theme.breakpoints.up("sm")]: {
         width: "12ch",
