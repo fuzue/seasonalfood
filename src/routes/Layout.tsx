2dd04fa05535f931bb23b1a21fe9197b28be46a7
Edgar Zanella Alvarenga <e@vaz.io>
2023-08-12 17:45:51 +0200

    change main colors back (ooops!)

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -30,8 +30,8 @@ import { useTranslation } from "react-i18next";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#000",
-      light: "#333",
+      main: "#3A6351",
+      light: "#F2EDD7",
       dark: "#393232", 
       
     },
