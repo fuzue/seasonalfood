e27e8a918366d527718991065b448d23149f63e0
Elena Volpato <talkto@elenavolpato.me>
2023-08-31 18:33:26 +0200

    adds translation to ContactDialog component

--- src/components/ContactDialog.tsx
+++ src/components/ContactDialog.tsx
@@ -3,19 +3,21 @@ import { DialogTitle, DialogContent, DialogContentText, Link, Box }
   from "@mui/material";
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { useTranslation } from "react-i18next";
 
 //dialog box that opens with each element clicked
 export default function ContactDialog() {
+  const { t } = useTranslation();
   return (
     <>
       <DialogTitle id="contact-dialog-title">
-        {"Contact"}
+        {t("ContactDialog_title")}
       </DialogTitle>
       <DialogContent>
         <DialogContentText id="contact-dialog-description"  color="dark-gray" >
-          Contact us via email, visit 
-            <Link underline="hover" color="inherit" href="https://seasonalfood.fuzue.tech/" target="_blank"> our website</Link> 
-            &nbsp;or check out <Link underline="hover" color="inherit" href="https://github.com/fuzue/seasonalfood"> our repository in github.</Link>
+          {t("ContactDialog_text1")}
+            <Link underline="hover" color="inherit" href="https://seasonalfood.fuzue.tech/" target="_blank"> {t("ContactDialog_text2")}</Link> 
+            &nbsp;{t( "ContactDialog_text3")} <Link underline="hover" color="inherit" href="https://github.com/fuzue/seasonalfood"> {t("ContactDialog_text4")}</Link>
           <Box sx={{display: 'flex', justifyContent: 'start', mt:2, }}>
             <AlternateEmailIcon sx={{mr:1}} />
             <Link underline="hover" color="inherit" href="mailto:contact@fuzue.tech">contact@fuzue.tech</Link>
