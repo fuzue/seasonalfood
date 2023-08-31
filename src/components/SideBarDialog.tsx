6be71c70c6f7fa27020cc3504fc530afae9e6b03
Elena Volpato <talkto@elenavolpato.me>
2023-08-31 17:40:14 +0200

    adds translation to SideBarDialog component

--- src/components/SideBarDialog.tsx
+++ src/components/SideBarDialog.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogActions, Button } from "@mui/material";
 import AboutDialog from "./AboutDialog";
 import ContactDialog from "./ContactDialog";
 import ContributeDialog from "./ContributeDialog";
+import { useTranslation } from "react-i18next";
 
 //dialog box that opens with each element clicked
 
@@ -10,6 +11,7 @@ export default function SideBarDialog(props: {
   dialogType: string;
   handleClose: () => void;
 }) {
+  const { t } = useTranslation();
   const renderDialog = (anchor: string) => {
     if (anchor === "about") {
       return <AboutDialog />;
@@ -25,7 +27,7 @@ export default function SideBarDialog(props: {
       {renderDialog(props.dialogType)}
       <DialogActions>
         <Button variant="outlined" onClick={props.handleClose} autoFocus>
-          close
+          {t("SideBarDialog_close")}
         </Button>
       </DialogActions>
     </Dialog>
