7bd95d4763c790caaf786ea28f2e1afd780b7e15
Elena Volpato <elena.volpato@digi-sapiens.de>
2025-07-14 17:19:29 +0200

    fixes links opening inside app

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -1,11 +1,20 @@
-
-import { DialogTitle, DialogContent, DialogContentText, Typography, Link }
-  from "@mui/material"
-import { useTranslation } from "react-i18next"
-
+import {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  Typography,
+  Link,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { Browser } from "@capacitor/browser";
 //dialog box that opens with each element clicked
 export default function AboutDialog() {
   const { t } = useTranslation();
+
+  const openExternalLink = async (url: string) => {
+    await Browser.open({ url });
+  };
+
   return (
     <>
       <DialogTitle id="about-dialog-title">
@@ -16,19 +25,35 @@ export default function AboutDialog() {
           <p>{t("AboutDialog_desc1")}</p>
           <p>{t("AboutDialog_desc2")}</p>
 
-          
-          <Typography sx={{fontWeight: 600 }}>
-          {t("AboutDialog_ItalyDisclaimer")}
+          <Typography sx={{ fontWeight: 600 }}>
+            {t("AboutDialog_ItalyDisclaimer")}
           </Typography>
-          <Typography sx={{fontWeight: 800, mt: 3}}>
-            {t("AboutDialog_aboutus")} <Link underline="hover" href="https://fuzue.tech" target="_blank">fuzue.tech</Link> 
-            <br /> 
-            {t("AboutDialog_aboutapp")} <Link underline="hover" href="https://seasonalfood.fuzue.tech/" target="_blank" > {t("AboutDialog_Here")}</Link> 
+          <Typography sx={{ fontWeight: 800, mt: 3 }}>
+            {t("AboutDialog_aboutus")}{" "}
+            <Link
+              underline="hover"
+              onClick={(e) => {
+                e.preventDefault();
+                openExternalLink("https://fuzue.tech/");
+              }}
+            >
+              fuzue.tech
+            </Link>
+            <br />
+            {t("AboutDialog_aboutapp")}{" "}
+            <Link
+              underline="hover"
+              onClick={(e) => {
+                e.preventDefault();
+                openExternalLink("https://seasonalfood.fuzue.tech/");
+              }}
+            >
+              {" "}
+              {t("AboutDialog_Here")}
+            </Link>
           </Typography>
         </DialogContentText>
       </DialogContent>
     </>
-
-
-  )
+  );
 }
--- src/components/ContactDialog.tsx
+++ src/components/ContactDialog.tsx
@@ -1,35 +1,77 @@
-
-import { DialogTitle, DialogContent, DialogContentText, Link, Box }
-  from "@mui/material";
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  Link,
+  Box,
+} from "@mui/material";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { useTranslation } from "react-i18next";
+import { Browser } from "@capacitor/browser";
 
 //dialog box that opens with each element clicked
 export default function ContactDialog() {
   const { t } = useTranslation();
+
+  const openExternalLink = async (url: string) => {
+    await Browser.open({ url });
+  };
   return (
     <>
       <DialogTitle id="contact-dialog-title">
         {t("ContactDialog_title")}
       </DialogTitle>
       <DialogContent>
-        <DialogContentText id="contact-dialog-description"  color="dark-gray" >
+        <DialogContentText id="contact-dialog-description" color="dark-gray">
           {t("ContactDialog_text1")}
-            <Link underline="hover" color="inherit" href="https://seasonalfood.fuzue.tech/" target="_blank">{/* {t("ContactDialog_text2")} */}</Link> 
-            &nbsp;{t( "ContactDialog_text3")} <Link underline="hover" sx={{fontWeight: 800}} href="https://github.com/fuzue/seasonalfood"> {t("ContactDialog_text4")}</Link>
-          <Box sx={{display: 'flex', justifyContent: 'start', mt:2, }}>
-            <AlternateEmailIcon sx={{mr:1}} />
-            <Link underline="hover" sx={{fontWeight: 800}} href="mailto:contact@fuzue.tech">contact@fuzue.tech</Link>
+          <Link
+            underline="hover"
+            color="inherit"
+            onClick={(e) => {
+              e.preventDefault();
+              openExternalLink("https://seasonalfood.fuzue.tech/");
+            }}
+          >
+            {/* {t("ContactDialog_text2")} */}
+          </Link>
+          &nbsp;{t("ContactDialog_text3")}{" "}
+          <Link
+            underline="hover"
+            sx={{ fontWeight: 800 }}
+            onClick={(e) => {
+              e.preventDefault();
+              openExternalLink("https://github.com/fuzue/seasonalfood");
+            }}
+          >
+            {" "}
+            {t("ContactDialog_text4")}
+          </Link>
+          <Box sx={{ display: "flex", justifyContent: "start", mt: 2 }}>
+            <AlternateEmailIcon sx={{ mr: 1 }} />
+            <Link
+              underline="hover"
+              sx={{ fontWeight: 800 }}
+              href="mailto:contact@fuzue.tech"
+            >
+              contact@fuzue.tech
+            </Link>
           </Box>
-          <Box sx={{display: 'flex', justifyContent: 'start', mt:1}}>
-            <GitHubIcon sx={{mr:1}}/>
-            <Link underline="hover" sx={{fontWeight: 800}}  href="https://github.com/fuzue/seasonalfood">GitHub Repo</Link>
+          <Box sx={{ display: "flex", justifyContent: "start", mt: 1 }}>
+            <GitHubIcon sx={{ mr: 1 }} />
+            <Link
+              underline="hover"
+              sx={{ fontWeight: 800 }}
+              onClick={(e) => {
+                e.preventDefault();
+                openExternalLink("https://github.com/fuzue/seasonalfood");
+              }}
+            >
+              GitHub Repo
+            </Link>
           </Box>
         </DialogContentText>
       </DialogContent>
     </>
-
-
-  )
+  );
 }
